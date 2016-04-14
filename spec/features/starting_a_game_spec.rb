5e8773f3b96bb8045146f17b5ab051ca1d4fc64a
Toby Clarke <teeohbee@gmail.com>
2016-04-14 21:48:47 +0100

    Add failing test for registering player name

--- spec/features/starting_a_game_spec.rb
+++ spec/features/starting_a_game_spec.rb
@@ -6,4 +6,12 @@ feature "Starting a new game" do
     click_button "New Game"
     expect(page).to have_content "Please enter your name"
   end
+
+  scenario "Once I enter my name, I can see my name" do
+    visit "/"
+    click_button "New Game"
+    fill_in('playername', :with => 'Toby')
+    click_button "Submit"
+    expect(page).to have_content "Welcome Toby"
+  end
 end
